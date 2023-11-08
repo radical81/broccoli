ebb426ce3d9f14ea6563992ead5ca6780561558e
Rex Jason Alobba <ralobba@gmail.com>
2023-11-08 13:39:14 +1100

    Initial Commit
     Initial commit
    
     Author:    Rex Jason Alobba <ralobba@gmail.com>
     Date:      Wed Nov 8 13:39:14 2023 +1100

---
+++ Broccoli/BroccoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroccoliApp.swift
+//  Broccoli
+//
+//  Created by Rex Jason Alobba on 8/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BroccoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
