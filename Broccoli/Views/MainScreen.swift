b7debf1fa81911d458e9cd627e08a1e05e500ba1
Rex Jason Alobba <ralobba@gmail.com>
2023-11-09 14:54:05 +1100

    Make sign up view a modal.
     Author:    Rex Jason Alobba <ralobba@gmail.com>
    
    Author:    Rex Jason Alobba <ralobba@gmail.com>

--- Broccoli/Views/MainScreen.swift
+++ Broccoli/Views/MainScreen.swift
@@ -1,17 +1,23 @@
 import SwiftUI
 
 struct MainScreen: View {
-  
+  @State var presentInvite: Bool = false
+
   var body: some View {
     VStack(spacing: 20) {
       Spacer()
       Text("Broccoli & Co.")
         .font(.largeTitle)
         .foregroundColor(.darkGreen)
-      SignUpView()
+      Button("Request an invite") {
+        presentInvite.toggle()
+      }
       .buttonStyle(.borderedProminent)
       .tint(.darkGreen)
       Spacer()
+    }
+    .sheet(isPresented: $presentInvite) {
+      SignUpView()
     }
   }
 }
