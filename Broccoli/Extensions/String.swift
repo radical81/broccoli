704cb232d1f8dbc0950c889588581aee26a605f6
Rex Jason Alobba <ralobba@gmail.com>
2023-11-10 10:24:38 +1100

    Amend documentation comments.

--- Broccoli/Extensions/String.swift
+++ Broccoli/Extensions/String.swift
@@ -2,8 +2,8 @@ import Foundation
 
 /// Convenience properties and methods for the String type.
 extension String {
-  /// Checks if the string is in a  valid email format.
+  /// Returns true if the string is in a  valid email format.
   var isValidEmail: Bool {
     NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
-  }  
+  }
 }
