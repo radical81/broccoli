6adf9e4926777a480cb57a6c4640055ba3b664a0
Rex Jason Alobba <ralobba@gmail.com>
2023-11-09 15:00:26 +1100

    Refactor subviews.
     Author:    Rex Jason Alobba <ralobba@gmail.com>
    
    Author:    Rex Jason Alobba <ralobba@gmail.com>

--- Broccoli/Views/SignUpView.swift
+++ Broccoli/Views/SignUpView.swift
@@ -3,34 +3,45 @@ import SwiftUI
 struct SignUpView: View {
   @State var name: String = ""
   @State var email: String = ""
-
+  
   var body: some View {
     VStack {
-      VStack(alignment: .leading) {
-        Text("Request an invite")
-          .font(.headline)
-        HStack {
-          Label("", systemImage: "person")
-          Spacer()
-          TextField("Name", text: $name)
-        }
-        HStack {
-          Label("", systemImage: "envelope")
-          Spacer()
-          TextField("Email address", text: $email)
-          
-        }
-      }
-      .padding()
-      .frame(maxWidth: 300)
-      .background(
-        RoundedRectangle(cornerRadius: 20)
-          .fill(.easyGreen)
-      )
+      form
     }
     Button("Submit") {
       
     }
+  }
+  
+  var nameField: some View {
+    HStack {
+      Label("", systemImage: "person")
+      Spacer()
+      TextField("Name", text: $name)
+    }
+  }
+  
+  var emailField: some View {
+    HStack {
+      Label("", systemImage: "envelope")
+      Spacer()
+      TextField("Email address", text: $email)
+    }
+  }
+
+  var form: some View {
+    VStack(alignment: .leading) {
+      Text("Request an invite")
+        .font(.headline)
+      nameField
+      emailField
+    }
+    .padding()
+    .frame(maxWidth: 300)
+    .background(
+      RoundedRectangle(cornerRadius: 20)
+        .fill(.easyGreen)
+    )
   }
 }
 
