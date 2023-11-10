589c2f6307781d0e8b03ee984f92cf96540dda34
Rex Jason Alobba <ralobba@gmail.com>
2023-11-10 17:12:24 +1100

    Simplify store.

--- Broccoli/Store/LocalStore.swift
+++ Broccoli/Store/LocalStore.swift
@@ -2,37 +2,11 @@ import Foundation
 
 /// Convenience functions for managing local store.
 struct LocalStore {
-  /// The key
+  /// The key to store if user has already registered.
   static let key = "registered"
 
-  /// Retrieve all registered emails.
-  private static var registeredEmails: [String] {
-    guard let registered = UserDefaults.standard.array(forKey: key) as? [String] else {
-      return []
-    }
-    return registered
-  }
-  
-  /// Add email to registry.
-  static func register(_ email: String) {
-    var registered = registeredEmails
-    if !registered.contains(email) {
-      registered.append(email)
-      UserDefaults.standard.set(registered, forKey: key)
-    }
-  }
-  
-  /// Remove email from registry.
-  static func cancelRegistration(_ email: String) {
-    var registered = registeredEmails
-    if registered.contains(email) {
-      registered.removeAll(where: { $0 == email })
-      UserDefaults.standard.set(registered, forKey: key)
-    }
-  }
-  
   /// True if email is already registered.
-  static func isRegistered(_ email: String) -> Bool {
-    registeredEmails.contains(email)
+  static var isRegistered: Bool {
+     UserDefaults.standard.bool(forKey: key)
   }
 }
