1af686f585e4bfd89643da0721c43d2ed0f64e1a
Rex Jason Alobba <ralobba@gmail.com>
2023-11-10 17:04:59 +1100

    Add function to remove registration.

--- Broccoli/Store/LocalStore.swift
+++ Broccoli/Store/LocalStore.swift
@@ -22,6 +22,15 @@ struct LocalStore {
     }
   }
   
+  /// Remove email from registry.
+  static func cancelRegistration(_ email: String) {
+    var registered = registeredEmails
+    if registered.contains(email) {
+      registered.removeAll(where: { $0 == email })
+      UserDefaults.standard.set(registered, forKey: key)
+    }
+  }
+  
   /// True if email is already registered.
   static func isRegistered(_ email: String) -> Bool {
     registeredEmails.contains(email)
