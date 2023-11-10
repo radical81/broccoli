d3ec78a12366bea5ca214ba3e57a603b77642632
Rex Jason Alobba <ralobba@gmail.com>
2023-11-10 11:05:15 +1100

    Add UI test for request an invite form display.

--- BroccoliUITests/BroccoliUITests.swift
+++ BroccoliUITests/BroccoliUITests.swift
@@ -10,4 +10,20 @@ final class BroccoliUITests: XCTestCase {
     XCTAssertTrue(mainHeading.exists)
     XCTAssertTrue(requestInviteButton.exists)
   }
+  
+  func testRequestAnInviteDisplay() throws {
+    app.launch()
+    let requestInviteButton = app.buttons["requestInviteButton"]
+    let requestInviteTitle = app.staticTexts["Request an invite"]
+    let nameTextField = app.textFields["nameTextField"]
+    let emailTextField = app.textFields["emailTextField"]
+    let confirmEmailTextField = app.textFields["confirmEmailTextField"]
+    let sendButton = app.buttons["sendButton"]
+    requestInviteButton.tap()
+    XCTAssertTrue(requestInviteTitle.exists)
+    XCTAssertTrue(nameTextField.exists)
+    XCTAssertTrue(emailTextField.exists)
+    XCTAssertTrue(confirmEmailTextField.exists)
+    XCTAssertTrue(sendButton.exists)    
+  }
 }
