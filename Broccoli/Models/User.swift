cb816fe5310ff8d3375419c9b1cb2acd75fe5358
Rex Jason Alobba <ralobba@gmail.com>
2023-11-21 12:28:32 +1100

    Add User model for registered users.

---
+++ Broccoli/Models/User.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// Represents already registered Users
+struct User: Identifiable, Codable {
+  let id: String
+  let name: String
+  let submittedAt: String
+  
+  // MARK: - Computed Properties
+//  var dateSubmitted: Date {
+//
+//  }
+}
